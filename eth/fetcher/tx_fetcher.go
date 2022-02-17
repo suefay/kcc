17d97298d1188c292727d142b99a2d693a7393b1
suefay <sufay30282@gmail.com>
2022-02-17 14:19:58 +0800

    decrease tx arrival timeout

--- eth/fetcher/tx_fetcher.go
+++ eth/fetcher/tx_fetcher.go
@@ -54,7 +54,7 @@ const (
 
 	// txArriveTimeout is the time allowance before an announced transaction is
 	// explicitly requested.
-	txArriveTimeout = 500 * time.Millisecond
+	txArriveTimeout = 100 * time.Millisecond
 
 	// txGatherSlack is the interval used to collate almost-expired announces
 	// with network fetches.
